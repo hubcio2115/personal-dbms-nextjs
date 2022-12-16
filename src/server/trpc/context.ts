b23e9b7cb5d4f25c9b476edf9cb34e0f7b8df8a6
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-16 15:11:25 +0100

    formatted context.ts

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,16 +1,16 @@
-import { type inferAsyncReturnType } from "@trpc/server";
-import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
-import { type Session } from "next-auth";
+import { type inferAsyncReturnType } from '@trpc/server';
+import { type CreateNextContextOptions } from '@trpc/server/adapters/next';
+import { type Session } from 'next-auth';
 
-import { getServerAuthSession } from "../common/get-server-auth-session";
-import { prisma } from "../db/client";
+import { getServerAuthSession } from '../common/get-server-auth-session';
+import { prisma } from '../db/client';
 
 type CreateContextOptions = {
   session: Session | null;
 };
 
 /** Use this helper for:
- * - testing, so we dont have to mock Next.js' req/res
+ * - testing, so we don't have to mock Next.js' req/res
  * - trpc's `createSSGHelpers` where we don't have req/res
  * @see https://create.t3.gg/en/usage/trpc#-servertrpccontextts
  **/
