f31dce1a3d6ad1f092423fe9fab3d2c602b81992
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-16 15:11:11 +0100

    added NavBar and PersonalDataCard to Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import { type NextPage } from 'next';
 import Head from 'next/head';
+import { clsx } from 'clsx';
 
+import PersonalDataCard from '../components/PersonalDataCard';
 import { trpc } from '../utils/trpc';
+import NavBar from '../components/NavBar';
 
 const Home: NextPage = () => {
   const { data } = trpc.personalData.getAll.useQuery();
@@ -13,15 +16,22 @@ const Home: NextPage = () => {
         <meta name="description" content="Project I've made" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <ol>
-            {data?.map((personalData) => (
-              <li key={personalData.id}>
-                <p>{personalData.firstName}</p>
-              </li>
-            )) ?? <li>Loading...</li>}
-          </ol>
+      <nav>
+        <NavBar />
+      </nav>
+
+      <main className="min-h-screen">
+        <div
+          className={clsx(
+            !!data
+              ? 'gird-cols-1 container mx-auto inline-grid gap-4 py-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'
+              : 'flex items-center justify-center',
+            'container min-h-full min-w-full',
+          )}
+        >
+          {data?.map((personalData) => (
+            <PersonalDataCard key={personalData.id} {...personalData} />
+          )) ?? <progress className="progress progress-primary w-56" />}
         </div>
       </main>
     </>
