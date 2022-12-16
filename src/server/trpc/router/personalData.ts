90593db9ecd0d04161eb8df5a5c5361de0570ecf
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-16 15:09:23 +0100

    added byId route

--- src/server/trpc/router/personalData.ts
+++ src/server/trpc/router/personalData.ts
@@ -1,7 +1,12 @@
 import { router, publicProcedure } from '../trpc';
+import { z } from 'zod';
 
 export const personalDataRouter = router({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.personalData.findMany();
+  }),
+
+  byId: publicProcedure.input(z.string()).query(({ ctx, input }) => {
+    return ctx.prisma.personalData.findFirst({ where: { id: input } });
   }),
 });
