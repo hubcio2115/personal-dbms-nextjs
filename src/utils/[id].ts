e55e787d698b6deb5e5bb5c985a71edc1a1c36f0
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-17 15:11:22 +0100

    added utils for [id]

---
+++ src/utils/[id].ts
@@ -0,0 +1,11 @@
+export const capitalize = (str: string) =>
+  str.charAt(0).toUpperCase() + str.slice(1);
+
+export const splitLowerCamelCase = (str: string) => {
+  const split = str.replace(/([a-z])([A-Z])/g, '$1 $2').split(' ');
+
+  if (split.length === 1) return capitalize(split[0] ?? '');
+  if (split.length === 2) return `${capitalize(split[0] ?? '')} ${split[1]}`;
+
+  return str;
+};
