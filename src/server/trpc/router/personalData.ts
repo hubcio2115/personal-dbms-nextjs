049e310fa56106fbbdfce4bd06b0d821b982a2a2
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-17 12:32:28 +0100

    created public routed for full CRUD

--- src/server/trpc/router/personalData.ts
+++ src/server/trpc/router/personalData.ts
@@ -1,12 +1,37 @@
 import { router, publicProcedure } from '../trpc';
 import { z } from 'zod';
+import {
+  basePersonalDataSchema,
+  personalDataSchemaWithId,
+} from '../../../types/personal-data';
 
 export const personalDataRouter = router({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.personalData.findMany();
-  }),
+  getAll: publicProcedure.query(({ ctx }) =>
+    ctx.prisma.personalData.findMany(),
+  ),
 
-  byId: publicProcedure.input(z.string()).query(({ ctx, input }) => {
-    return ctx.prisma.personalData.findFirst({ where: { id: input } });
-  }),
+  byId: publicProcedure
+    .input(z.string())
+    .query(({ ctx, input: id }) =>
+      ctx.prisma.personalData.findFirst({ where: { id } }),
+    ),
+
+  delete: publicProcedure
+    .input(z.string())
+    .query(({ ctx, input: id }) =>
+      ctx.prisma.personalData.delete({ where: { id } }),
+    ),
+
+  update: publicProcedure
+    .input(personalDataSchemaWithId)
+    .query(({ ctx, input: data }) =>
+      ctx.prisma.personalData.update({
+        where: { id: data.id },
+        data,
+      }),
+    ),
+
+  create: publicProcedure
+    .input(basePersonalDataSchema)
+    .query(({ ctx, input: data }) => ctx.prisma.personalData.create({ data })),
 });
