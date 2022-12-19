7f7721077a399023e3209bcd358326225b51054d
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-19 21:49:18 +0100

    added logout button to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,25 @@
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { type FC } from 'react';
 
-const NavBar: FC = () => (
-  <div className="navbar bg-neutral">
-    <Link href="/" className="btn-ghost btn text-xl normal-case">
-      Personal Data DBMS
-    </Link>
-  </div>
-);
+const NavBar: FC = () => {
+  const { status } = useSession();
+
+  return (
+    <div className="navbar bg-neutral">
+      <div className="flex-1">
+        <Link href="/" className="btn-ghost btn text-xl normal-case">
+          Personal Data DBMS
+        </Link>
+      </div>
+
+      {status === 'authenticated' ? (
+        <button className="btn-error btn" onClick={() => signOut()}>
+          Log out
+        </button>
+      ) : null}
+    </div>
+  );
+};
 
 export default NavBar;
