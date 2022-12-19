aeb829d502f71edbb2252d17d21a13db89aeb307
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-19 13:45:00 +0100

    added authorization

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,22 +1,69 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-// Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { verify } from 'argon2';
 
-import { prisma } from "../../../server/db/client";
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+import { prisma } from '../../../server/db/client';
+import { loginSchema } from '../../../server/common/auth';
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
     session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
+      if (!!session.user) session.user.id = user.id;
+
       return session;
     },
+    jwt: async ({ token, user }) => {
+      if (!!user) token.id = user.id;
+
+      return token;
+    },
+  },
+  jwt: {
+    maxAge: 1 * 24 * 30 * 60,
+  },
+  pages: {
+    signIn: '/admin',
   },
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
+    Credentials({
+      name: 'credentials',
+      credentials: {
+        username: {
+          label: 'username',
+          type: 'text',
+          placeholder: 'username',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'password',
+        },
+      },
+      authorize: async (credentials) => {
+        const creds = await loginSchema.parse(credentials);
+
+        const user = await prisma.user.findFirst({
+          where: { username: creds.username },
+        });
+
+        if (!user) return null;
+
+        const isValidPassword = await verify(user.password, creds.password);
+
+        if (!isValidPassword) return null;
+
+        return {
+          id: user.id,
+          username: user.username,
+        };
+      },
+    }),
     // ...add more providers here
   ],
 };
