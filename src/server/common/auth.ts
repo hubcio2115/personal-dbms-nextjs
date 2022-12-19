ab82a1e9b48696a1754211c1554bce4325075b53
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-19 13:35:48 +0100

    created login schema

---
+++ src/server/common/auth.ts
@@ -0,0 +1,8 @@
+import * as z from 'zod';
+
+export const loginSchema = z.object({
+  username: z.string(),
+  password: z.string().min(4).max(20),
+});
+
+export type Login = z.infer<typeof loginSchema>;
