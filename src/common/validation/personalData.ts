d8fc9b53fe17d7e782aec6a15565e7ebf2411ab3
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-21 10:54:43 +0100

    created schema for PersonalData

---
+++ src/common/validation/personalData.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+export const personalDataSchema = z.object({
+  id: z.string(),
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+  maidenName: z.string().min(1),
+  age: z.number().min(0),
+  sex: z.enum(['female', 'male']),
+  email: z.string().email(),
+  phone: z.string().min(1),
+  username: z.string().min(1),
+  birthDate: z.date(),
+  bloodGroup: z.custom<`${'A' | 'B' | 'O'}${'-' | '+'}`>((val) =>
+    /^[ABO][+-]/g.test(val as string),
+  ),
+  height: z.number().min(0),
+  weight: z.number().min(0),
+  eyeColor: z.string().min(1),
+});
