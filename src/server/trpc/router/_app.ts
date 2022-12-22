9a7816e4f8676eb18144a4b5cd29f1f65a2b206a
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-22 16:12:44 +0100

    deleted authRouter

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,8 @@
 import { router } from '../trpc';
-import { authRouter } from './auth';
 import { personalDataRouter } from './personalData';
 
 export const appRouter = router({
   personalData: personalDataRouter,
-  auth: authRouter,
 });
 
 // export type definition of API
