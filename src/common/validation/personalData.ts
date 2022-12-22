9277b16108e4a901dcf1a4ae310ce5ba535fdcfc
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-22 18:36:10 +0100

    added error messages to validations

--- src/common/validation/personalData.ts
+++ src/common/validation/personalData.ts
@@ -1,22 +1,28 @@
 import { z } from 'zod';
 
+const requiredErrorMessage = 'This field cannot be empty!';
+const negativeErrorMessage = 'This value has to be positive!';
+
 export const personalDataSchema = z.object({
   id: z.string(),
-  firstName: z.string().min(1),
-  lastName: z.string().min(1),
-  maidenName: z.string().min(1),
-  age: z.number().min(0),
+  firstName: z.string().min(1, requiredErrorMessage),
+  lastName: z.string().min(1, requiredErrorMessage),
+  maidenName: z.string().min(1, requiredErrorMessage),
+  age: z.number().min(0, negativeErrorMessage),
   sex: z.enum(['female', 'male']),
-  email: z.string().email(),
-  phone: z.string().min(1),
-  username: z.string().min(1),
-  birthDate: z.date(),
-  bloodGroup: z.custom<`${'A' | 'B' | 'O'}${'-' | '+'}`>((val) =>
-    /^[ABO][+-]/g.test(val as string),
+  email: z.string().email('Provided value is not in format of an email!'),
+  phone: z.string().min(1, requiredErrorMessage),
+  username: z.string().min(1, requiredErrorMessage),
+  birthDate: z.date().max(new Date()),
+  bloodGroup: z.custom<
+    `${'A' | 'B' | 'O'}${'-' | '+'}` | 'O' | `AB${'-' | '+'}`
+  >(
+    (val) => /^([ABO]|AB)[+-]|O/g.test(val as string),
+    'You provided non existent blood type!',
   ),
-  height: z.number().min(0),
-  weight: z.number().min(0),
-  eyeColor: z.string().min(1),
+  height: z.number().min(0, negativeErrorMessage),
+  weight: z.number().min(0, negativeErrorMessage),
+  eyeColor: z.string().min(1, requiredErrorMessage),
 });
 export type PersonalDataSchema = z.infer<typeof personalDataSchema>;
 
