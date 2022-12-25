697ae2856aee1a412fb6e9f69ff9e9d8b73f103f
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-25 20:46:40 +0100

    updated schema after changing prisma

--- src/common/validation/personalData.ts
+++ src/common/validation/personalData.ts
@@ -10,19 +10,7 @@ export const personalDataSchema = z.object({
   maidenName: z.string().min(1, requiredErrorMessage),
   age: z.number().min(0, negativeErrorMessage),
   sex: z.enum(['female', 'male']),
-  email: z.string().email('Provided value is not in format of an email!'),
   phone: z.string().min(1, requiredErrorMessage),
-  username: z.string().min(1, requiredErrorMessage),
-  birthDate: z.date().max(new Date()),
-  bloodGroup: z.custom<
-    `${'A' | 'B' | 'O'}${'-' | '+'}` | 'O' | `AB${'-' | '+'}`
-  >(
-    (val) => /^([ABO]|AB)[+-]|O/g.test(val as string),
-    'You provided non existent blood type!',
-  ),
-  height: z.number().min(0, negativeErrorMessage),
-  weight: z.number().min(0, negativeErrorMessage),
-  eyeColor: z.string().min(1, requiredErrorMessage),
 });
 export type PersonalDataSchema = z.infer<typeof personalDataSchema>;
 
