7e7af30245e4c9e541fdda0f2356fb945d154fd3
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-25 20:46:27 +0100

    changed schema for logging in

--- src/common/validation/auth.ts
+++ src/common/validation/auth.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  username: z.string(),
-  password: z.string().min(4).max(20),
+  email: z.string().email(),
+  password: z.string().min(8),
 });
 
 export type Login = z.infer<typeof loginSchema>;
