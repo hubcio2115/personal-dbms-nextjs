c9136b98f181aae73247150170308225a6d85f8e
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-31 22:51:13 +0100

    outscoped LoginForm

---
+++ src/components/forms/LoginForm.tsx
@@ -0,0 +1,93 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { signIn } from 'next-auth/react';
+import { type FC, useCallback } from 'react';
+import { useForm } from 'react-hook-form';
+import { type Login, loginSchema } from '../../common/validation/auth';
+import Link from 'next/link';
+import clsx from 'clsx';
+
+const LoginForm: FC = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<Login>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = useCallback((data: Login) => {
+    try {
+      signIn('credentials', { ...data, callbackUrl: '/dashboard' });
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  return (
+    <form
+      className="flex w-full flex-auto items-center justify-center"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="card w-96 bg-primary shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title text-primary-content">Login Page</h2>
+
+          <div className="form-control">
+            <label className="label text-primary-content">
+              <span className="label-text">Email:</span>
+            </label>
+
+            <input
+              type="text"
+              className={clsx(
+                'input',
+                !!errors.email ? 'input-bordered input-error' : '',
+              )}
+              {...register('email')}
+            />
+
+            <label className="label">
+              {!!errors.email ? (
+                <span className="label-text-alt">{errors.email.message}</span>
+              ) : null}
+            </label>
+          </div>
+
+          <div className="form-control">
+            <input
+              type="password"
+              placeholder="Type your password..."
+              className={clsx(
+                'input',
+                !!errors.password ? 'input-bordered input-error' : '',
+              )}
+              {...register('password')}
+            />
+
+            <label className="label">
+              {!!errors.password ? (
+                <span className="label-text-alt">
+                  {errors.password.message}
+                </span>
+              ) : null}
+            </label>
+          </div>
+
+          <div className="card-actions items-center justify-between">
+            <Link href="/register">Don&apos;t have account yet?</Link>
+
+            <button className="btn-success btn" type="submit">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,102 +1,25 @@
 import type { GetServerSideProps, NextPage } from 'next';
-import { useForm } from 'react-hook-form';
-import { type Login, loginSchema } from '../common/validation/auth';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { getSession, signIn } from 'next-auth/react';
-import { useCallback } from 'react';
+import { getSession } from 'next-auth/react';
 import Head from 'next/head';
 import MainLayout from '../layouts/MainLayout';
-import Link from 'next/link';
+import LoginForm from '../components/forms/LoginForm';
+import { redirectIfSession } from '../utils/redirectIfSession';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = await getSession(ctx);
-
-  return !!session
-    ? {
-        redirect: {
-          destination: '/dashboard',
-          permanent: false,
-        },
-      }
-    : {
-        props: {
-          session,
-          query: ctx.query,
-        },
-      };
+  return redirectIfSession(session, true, '/dashboard', ctx);
 };
 
-const Home: NextPage = () => {
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm<Login>({
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-    resolver: zodResolver(loginSchema),
-  });
+const Home: NextPage = () => (
+  <MainLayout>
+    <Head>
+      <title>Login Page</title>
+    </Head>
 
-  const onSubmit = useCallback((data: Login) => {
-    try {
-      signIn('credentials', { ...data, callbackUrl: '/dashboard' });
-    } catch (err) {
-      console.error(err);
-    }
-  }, []);
-
-  return (
-    <>
-      <Head>
-        <title>Login Page</title>
-      </Head>
-
-      <MainLayout>
-        <form
-          className="flex w-full flex-auto items-center justify-center"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className="card w-96 bg-primary shadow-xl">
-            <div className="card-body">
-              <h2 className="card-title text-primary-content">Login Page</h2>
-
-              <div>
-                <input
-                  type="text"
-                  placeholder="Type your username..."
-                  className="input-bordered input w-full max-w-xs"
-                  {...register('email')}
-                />
-
-                {!!errors.email ? <p>{errors.email.message}</p> : null}
-              </div>
-
-              <div>
-                <input
-                  type="password"
-                  placeholder="Type your password..."
-                  className="input-bordered input my-2 w-full max-w-xs"
-                  {...register('password')}
-                />
-
-                {!!errors.password ? <p>{errors.password.message}</p> : null}
-              </div>
-
-              <div className="card-actions items-center justify-between">
-                <Link href="/register">Don&apos;t have account yet?</Link>
-
-                <button className="btn-success btn" type="submit">
-                  Login
-                </button>
-              </div>
-            </div>
-          </div>
-        </form>
-      </MainLayout>
-    </>
-  );
-};
+    <MainLayout>
+      <LoginForm />
+    </MainLayout>
+  </MainLayout>
+);
 
 export default Home;
