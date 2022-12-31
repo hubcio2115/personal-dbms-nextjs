a37eaf986a182a6edccbed01a24b054c67724725
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-12-31 22:56:35 +0100

    added proper login form errors

--- src/common/validation/auth.ts
+++ src/common/validation/auth.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
+  email: z.string().email('Invalid email form!'),
+  password: z.string().min(8, 'Provided password is too short!'),
 });
 
 export type Login = z.infer<typeof loginSchema>;
