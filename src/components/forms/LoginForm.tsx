12e8a2015c1febd8130541536a0bf1b7e1fd304a
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-01-01 18:43:53 +0100

    added env check for logging errors

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -24,7 +24,7 @@ const LoginForm: FC = () => {
     try {
       signIn('credentials', { ...data, callbackUrl: '/dashboard' });
     } catch (err) {
-      console.error(err);
+      if (process.env.NODE_ENV === 'development') console.error(err);
     }
   }, []);
 
