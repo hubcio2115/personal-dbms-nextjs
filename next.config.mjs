2fcc56c320cb54fac90925a89d8acfad067ac5be
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-01-14 20:13:50 +0100

    added .env validation

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,12 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import('./src/env/server.mjs'));
+const { NODE_ENV, ...env } = (await import('./src/env/server.mjs')).env;
+!process.env.SKIP_ENV_VALIDATION && env;
 
 /** @type {import("next").NextConfig} */
 const config = {
+  env,
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
